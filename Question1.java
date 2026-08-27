c9bd804853a484bd91dc619c5e649419cb8eab87
Mehfila123 <mehfilaparkkulthil@gmail.com>
2026-08-27 17:06:26 +0530

    added question list

--- Question1.java
+++ Question1.java
@@ -6,6 +6,9 @@
 public class Question1{
     public static void main(String[]args){
         System.out.println("Good Morning");
+        for(int i=2;i<=100;i++){
+            System.out.println("| " + i + " | | " + " [Click here](https://github.com/Mehfila-Parkkulthil/Java--Questions/blob/main/Question"+i+".java)"+ " ||");
+        }
     }
 }
  
